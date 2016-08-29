d1d02e166e52c578446db4bba93f8c9c930d7c1c
lucasart <lucas.braesch@gmail.com>
2016-08-29 12:33:40 +0800

    Random pruning

--- generate.py
+++ generate.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
-from __future__ import print_function  # Python 2.7 compatibility
-{'You need Python 2.7+ or Python 3.1+'}  # Syntax error on earlier versions
-
-import sys
+import random
 import subprocess
+import sys
+
 import chess
 
 # Configure here
 Chess960 = True
 Engine = '../Stockfish/master'
 Options = {'Hash': 64, 'Threads': 7}
-Cutoff = 50  # expressed in cp
+Pruning = {'score': 50, 'random': 0.5}
 TimeControl = {'depth': 13, 'nodes': None, 'movetime': 1000}
+
 
 class UCIEngine():
     def __init__(self, engine):
@@ -69,7 +69,7 @@ class UCIEngine():
                         if len(tokens) == 2 or not tokens[2].endswith('bound'):
                             score = int(tokens[1])
                     elif tokens[0] == 'mate':
-                        score = math.copysign(Cutoff, int(tokens[1]))
+                        score = math.copysign(Pruning['score'], int(tokens[1]))
             elif line.startswith('bestmove'):
                 return line.split()[1], score
 
@@ -90,6 +90,9 @@ if __name__ == '__main__':
             print('processing:', fen)
 
             for move in board.legal_moves:
+                if random.random() < Pruning['random']:
+                    continue
+
                 board.push(move)
                 childFen = board.shredder_fen() if Chess960 else board.fen()
                 board.pop()
@@ -98,7 +101,7 @@ if __name__ == '__main__':
                 uciEngine.isready()
                 bestmove, score = uciEngine.go(TimeControl)
 
-                if abs(score) < Cutoff:
+                if abs(score) < Pruning['score']:
                     print(childFen, file=outFile)
 
     uciEngine.quit()
